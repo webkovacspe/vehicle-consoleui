8d47f2bffa6dc81146f44117a89d1073174fec39
webkovacs <webkovacs@gmail.com>
2023-01-22 19:09:32 +0100

    keresés rendszám alapján

--- src/main/java/hu/kovacspeterzoltan/bootcamp/vehicleconsoleui/controller/ConsoleUIController.java
+++ src/main/java/hu/kovacspeterzoltan/bootcamp/vehicleconsoleui/controller/ConsoleUIController.java
@@ -1,25 +1,79 @@
 package hu.kovacspeterzoltan.bootcamp.vehicleconsoleui.controller;
 
 import hu.kovacspeterzoltan.bootcamp.vehicleregister.dao.VehicleRegisterInteractorInterface;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Scanner;
 
 public class ConsoleUIController {
     VehicleRegisterInteractorInterface registerInteractor;
+    Scanner scanner;
 
     public void setRegisterInteractorImp(VehicleRegisterInteractorInterface registerInteractorImp) {
         registerInteractor = registerInteractorImp;
     }
     public void start() {
-        String jsonString = """
-            {
-              "registrationNumber":"XX:YY-999",
-              "vehicleRegister":"Cseresznye",
-              "vehicle":"vehicle",
-              "make":"Opel",
-              "model":"Astra",
-              "numberOfSeats":5,
-              "vehicleType":"m1"
+        scanner = new Scanner(System.in);
+        String action = null;
+        do {
+            action = getAction();
+
+            if ("r".equals(action)) {
+                saveAction();
+            } else if ("l".equals(action)) {
+                findAction();
             }
-            """;
-        registerInteractor.saveVehicle(jsonString);
+        } while (!"k".equals(action));
+
+        System.out.println("Sikeres kilépés");
+    }
+
+    private void findAction() {
+        try {
+            JSONObject jsonObject = new JSONObject();
+            System.out.println("Adja meg a rendszámot:");
+            jsonObject.put("registrationNumber", scanner.next());
+
+            registerInteractor.getVehicleByRegisterNumber(jsonObject.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+    private void saveAction() {
+        try {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("vehicle", "vehicle");
+
+            System.out.println("Adja meg a rendszámot:");
+            jsonObject.put("registrationNumber", scanner.next());
+            System.out.println("Adja meg a nyilvántartó nevét:");
+            jsonObject.put("vehicleRegister", scanner.next());
+            System.out.println("Adja meg a gyártmányt:");
+            jsonObject.put("make", scanner.next());
+            System.out.println("Adja meg a típust:");
+            jsonObject.put("model", scanner.next());
+            System.out.println("Adja meg az ülőhelyek számát:");
+            jsonObject.put("numberOfSeats", scanner.next());
+            System.out.println("Adja meg a jármű kategóriáját:");
+            jsonObject.put("vehicleType", scanner.next());
+
+            registerInteractor.saveVehicle(jsonObject.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getAction() {
+        String action;
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+        System.out.println("Válasszon funkciót: Rögzít (r); Lekérdez (l); Kilép (k)");
+        action = scanner.next();
+        return action;
+    }
+
+    private boolean isValidAction(String action) {
+        return ("r".equals(action) || "l".equals(action) || "k".equals(action));
     }
 }
--- src/main/java/hu/kovacspeterzoltan/bootcamp/vehicleconsoleui/dao/ConsoleUIRegisterPresenterImp.java
+++ src/main/java/hu/kovacspeterzoltan/bootcamp/vehicleconsoleui/dao/ConsoleUIRegisterPresenterImp.java
@@ -1,10 +1,39 @@
 package hu.kovacspeterzoltan.bootcamp.vehicleconsoleui.dao;
 
 import hu.kovacspeterzoltan.bootcamp.vehicleregister.dao.VehicleRegisterPresenterInterface;
+import hu.kovacspeterzoltan.bootcamp.vehicleregister.parser.InvalidJsonStringException;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ConsoleUIRegisterPresenterImp implements VehicleRegisterPresenterInterface {
     @Override
-    public void displaySaveMessage(String responseMessage) {
+    public void displayMessage(String responseMessage) {
         System.out.println(responseMessage);
+    }
+
+    @Override
+    public void displayJsonRequest(String jsonString) {
+        try {
+            JSONObject jsonObject = new JSONObject(jsonString);
+            String message = """
+                A keresett jármű adatai:
+                rendszám: %s
+                nyilvántartó neve: %s
+                gyártmány: %s
+                típus: %s
+                ülőhelyek száma: %d
+                jármű kategóriája: %s
+            """;
+            System.out.println(message.formatted(
+                    jsonObject.get("registrationNumber"),
+                    jsonObject.get("vehicleRegister"),
+                    jsonObject.get("make"),
+                    jsonObject.get("model"),
+                    jsonObject.get("numberOfSeats"),
+                    jsonObject.get("vehicleType")
+            ));
+        } catch (JSONException e) {
+            throw new InvalidJsonStringException();
+        }
     }
 }
